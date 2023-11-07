79490e46d59f86358e17a8d376929e76a181da3e
Juan Felipe Caraballo <69644034+juanfelipecaraballo@users.noreply.github.com>
2023-11-07 18:26:38 -0500

    Consultas avanzadas parte 2, 9 y 10 fin

--- src/main/java/uniandes/edu/co/proyecto/controller/ConsultasAvanzadasController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/ConsultasAvanzadasController.java
@@ -3,6 +3,9 @@ package uniandes.edu.co.proyecto.controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.sql.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -32,6 +35,7 @@ public class ConsultasAvanzadasController {
        String rol = usuarioRepository.mostrarRolUsuarioPorId(id);
        System.out.println("********************");
        System.out.println(rol);
+       model.addAttribute("rol", rol);
        if (rol.equals("Administrador")){
           return "consultasAvanzadasAdmin";
        }
@@ -40,6 +44,9 @@ public class ConsultasAvanzadasController {
        } 
        else if (rol.equals("Gerente")){
          return "consultasAvanzadasGer";
+       }
+       else if (rol.equals("GerenteGeneral")){
+         return "consultasAvanzadasGerGen";
        }
        else{
          return "consultasAvanzadasSinPermiso";
@@ -56,7 +63,67 @@ public class ConsultasAvanzadasController {
      @GetMapping("/consultasavanzadas/R9fechasid")
      public String consultasAvanzadasR9fechasid(@ModelAttribute fechas2 fechas, Model model){
         model.addAttribute("reservaservicios", consultasRepository.r9(fechas.getFecha1(), fechas.getFecha2()));
+         model.addAttribute("fecha1",fechas.getFecha1());
+         model.addAttribute("fecha2",fechas.getFecha2());
         return "consultasAvanzadasR9";
      }
 
+
+     //ordernar resultados req9 por fecha
+     @GetMapping("/consultasavanzadas/R9/mod1/{fecha1}/{fecha2}")
+     public String consultasAvanzadasR9mod1(@ModelAttribute("fecha1") Date fecha1, @ModelAttribute("fecha2") Date fecha2, Model model){
+      model.addAttribute("reservaservicios", consultasRepository.r9mod1(fecha1, fecha2));
+      return "consultasAvanzadasR9mod1";
+     }
+     //ordernar resultados req9 por el id del usuario
+     @GetMapping("/consultasavanzadas/R9/mod2/{fecha1}/{fecha2}")
+     public String consultasAvanzadasR9mod2(@ModelAttribute("fecha1") Date fecha1, @ModelAttribute("fecha2") Date fecha2, Model model){
+      model.addAttribute("reservaservicios", consultasRepository.r9mod2(fecha1, fecha2));
+      return "consultasAvanzadasR9mod2";
+     }
+
+     //ordernar resultados req9 por el nombre del usuario
+     @GetMapping("/consultasavanzadas/R9/mod3/{fecha1}/{fecha2}")
+     public String consultasAvanzadasR9mod3(@ModelAttribute("fecha1") Date fecha1, @ModelAttribute("fecha2") Date fecha2, Model model){
+      model.addAttribute("reservaservicios", consultasRepository.r9mod3(fecha1, fecha2));
+      return "consultasAvanzadasR9mod3";
+     }
+
+     //ordernar resultados req9 por el nombre del servicio
+     @GetMapping("/consultasavanzadas/R9/mod4/{fecha1}/{fecha2}")
+     public String consultasAvanzadasR9mod4(@ModelAttribute("fecha1") Date fecha1, @ModelAttribute("fecha2") Date fecha2, Model model){
+      model.addAttribute("reservaservicios", consultasRepository.r9mod4(fecha1, fecha2));
+      return "consultasAvanzadasR9mod4";
+     }
+
+
+
+
+     @GetMapping("/consultasavanzadas/R10")
+     public String consultasAvanzadasR10(Model model){
+        model.addAttribute("fechas", new fechas2());
+        return "consultasAvanzadasR10Nuevo";
+     }
+
+     @GetMapping("/consultasavanzadas/R10fechasid")
+     public String consultasAvanzadasR10fechasid(@ModelAttribute fechas2 fechas, Model model){
+        model.addAttribute("reservaservicios", consultasRepository.r10(fechas.getFecha1(), fechas.getFecha2()));
+         model.addAttribute("fecha1",fechas.getFecha1());
+         model.addAttribute("fecha2",fechas.getFecha2());
+        return "consultasAvanzadasR10";
+     }
+
+     //ordernar resultados req10 por nombre usuario
+     @GetMapping("/consultasavanzadas/R10/mod1/{fecha1}/{fecha2}")
+     public String consultasAvanzadasR10mod1(@ModelAttribute("fecha1") Date fecha1, @ModelAttribute("fecha2") Date fecha2, Model model){
+      model.addAttribute("reservaservicios", consultasRepository.r10mod1(fecha1, fecha2));
+      return "consultasAvanzadasR10mod1";
+     }
+
+     //ordernar resultados req10 por id usuario
+     @GetMapping("/consultasavanzadas/R10/mod2/{fecha1}/{fecha2}")
+     public String consultasAvanzadasR10mod2(@ModelAttribute("fecha1") Date fecha1, @ModelAttribute("fecha2") Date fecha2, Model model){
+      model.addAttribute("reservaservicios", consultasRepository.r10mod2(fecha1, fecha2));
+      return "consultasAvanzadasR10mod2";
+     }
 }
--- src/main/java/uniandes/edu/co/proyecto/repositorio/ConsultasAvanzadasRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/ConsultasAvanzadasRepository.java
@@ -21,4 +21,82 @@ public interface ConsultasAvanzadasRepository extends JpaRepository<ReservaServ,
                     +"WHERE rs.fechainicio >= :fecha1 AND rs.fechafin <= :fecha2", nativeQuery = true)
     List<Object[]> r9(@Param("fecha1") Date fecha1, @Param("fecha2") Date fecha2);               
 
+    //req 9 mod 1
+    @Query(value ="SELECT rs.id, rs.fechafin, rs.nombre_servicio, u.id, u.nombre FROM " 
+                    +"RESERVASERVICIOS rs JOIN " 
+                    +"OFERTASHABITACIONES oh ON rs.habitacion_id  = oh.id "
+                    +"JOIN USUARIOS u "
+                    +"ON u.id = oh.usuarios_id "
+                    +"WHERE rs.fechainicio >= :fecha1 AND rs.fechafin <= :fecha2 "
+                    +"ORDER BY rs.fechafin DESC", nativeQuery = true)
+    List<Object[]> r9mod1(@Param("fecha1") Date fecha1, @Param("fecha2") Date fecha2);
+    
+    //req 9 mod 2
+    @Query(value ="SELECT rs.id, rs.fechafin, rs.nombre_servicio, u.id, u.nombre FROM " 
+                    +"RESERVASERVICIOS rs JOIN " 
+                    +"OFERTASHABITACIONES oh ON rs.habitacion_id  = oh.id "
+                    +"JOIN USUARIOS u "
+                    +"ON u.id = oh.usuarios_id "
+                    +"WHERE rs.fechainicio >= :fecha1 AND rs.fechafin <= :fecha2 "
+                    +"ORDER BY u.id DESC", nativeQuery = true)
+    List<Object[]> r9mod2(@Param("fecha1") Date fecha1, @Param("fecha2") Date fecha2);
+
+
+    //req 9 mod 3
+    @Query(value ="SELECT rs.id, rs.fechafin, rs.nombre_servicio, u.id, u.nombre FROM " 
+                    +"RESERVASERVICIOS rs JOIN " 
+                    +"OFERTASHABITACIONES oh ON rs.habitacion_id  = oh.id "
+                    +"JOIN USUARIOS u "
+                    +"ON u.id = oh.usuarios_id "
+                    +"WHERE rs.fechainicio >= :fecha1 AND rs.fechafin <= :fecha2 "
+                    +"ORDER BY u.nombre DESC", nativeQuery = true)
+    List<Object[]> r9mod3(@Param("fecha1") Date fecha1, @Param("fecha2") Date fecha2);
+
+    //req 9 mod 4
+    @Query(value ="SELECT rs.id, rs.fechafin, rs.nombre_servicio, u.id, u.nombre FROM " 
+                    +"RESERVASERVICIOS rs JOIN " 
+                    +"OFERTASHABITACIONES oh ON rs.habitacion_id  = oh.id "
+                    +"JOIN USUARIOS u "
+                    +"ON u.id = oh.usuarios_id "
+                    +"WHERE rs.fechainicio >= :fecha1 AND rs.fechafin <= :fecha2 "
+                    +"ORDER BY rs.nombre_servicio DESC", nativeQuery = true)
+    List<Object[]> r9mod4(@Param("fecha1") Date fecha1, @Param("fecha2") Date fecha2);
+
+
+
+    //req 10
+    @Query(value ="SELECT u1.id, u1.nombre, u1.apellido FROM usuarios u1\r\n" + //
+            "WHERE u1.id NOT IN (\r\n" + //
+            "SELECT u.id FROM \r\n" + //
+            "RESERVASERVICIOS rs JOIN \r\n" + //
+            "OFERTASHABITACIONES oh ON rs.habitacion_id  = oh.id\r\n" + //
+            "JOIN USUARIOS u\r\n" + //
+            "ON u.id = oh.usuarios_id\r\n" + //
+            "WHERE rs.fechainicio >= '01-FEB-2023' AND rs.fechafin <= '04-FEB-2023')", nativeQuery = true)
+    List<Object[]> r10(@Param("fecha1") Date fecha1, @Param("fecha2") Date fecha2); 
+
+    //req 10 mod 1
+    @Query(value ="SELECT u1.id, u1.nombre, u1.apellido FROM usuarios u1\r\n" + //
+            "WHERE u1.id NOT IN (\r\n" + //
+            "SELECT u.id FROM \r\n" + //
+            "RESERVASERVICIOS rs JOIN \r\n" + //
+            "OFERTASHABITACIONES oh ON rs.habitacion_id  = oh.id\r\n" + //
+            "JOIN USUARIOS u\r\n" + //
+            "ON u.id = oh.usuarios_id\r\n" + //
+            "WHERE rs.fechainicio >= :fecha1 AND rs.fechafin <= :fecha2) "
+            +"ORDER BY u1.nombre", nativeQuery = true)
+    List<Object[]> r10mod1(@Param("fecha1") Date fecha1, @Param("fecha2") Date fecha2);
+
+
+    //req 10 mod 2
+    @Query(value ="SELECT u1.id, u1.nombre, u1.apellido FROM usuarios u1\r\n" + //
+            "WHERE u1.id NOT IN (\r\n" + //
+            "SELECT u.id FROM \r\n" + //
+            "RESERVASERVICIOS rs JOIN \r\n" + //
+            "OFERTASHABITACIONES oh ON rs.habitacion_id  = oh.id\r\n" + //
+            "JOIN USUARIOS u\r\n" + //
+            "ON u.id = oh.usuarios_id\r\n" + //
+            "WHERE rs.fechainicio >= :fecha1 AND rs.fechafin <= :fecha2) "
+            +"ORDER BY u1.id", nativeQuery = true)
+    List<Object[]> r10mod2(@Param("fecha1") Date fecha1, @Param("fecha2") Date fecha2);
 }
