ae1b8ed9d4f3cefe5d50c423678f4232a3fccf37
s.umanap <s.umanap@uniandes.edu.co>
2023-12-03 15:28:20 -0500

    servicios implementados

--- demo/src/main/java/com/example/demo/controller/servicioController.java
+++ demo/src/main/java/com/example/demo/controller/servicioController.java
@@ -24,16 +24,48 @@ public class servicioController {
     @Autowired
     private ServicioRepository servicioRepository;
 
-    @GetMapping("/servicio")
+    @GetMapping("/servicios")
     public String obtenerTodasLasBebidasTipos(Model model){
         model.addAttribute("servicios", servicioRepository.findAll());
-        return "servicio";
+        return "servicios";
     }
 
-    @GetMapping("/servicioForm")
-    public String mostrarFormulario(Model model) {
+    @GetMapping("/servicios/new")
+    public String serviciosForm(Model model) {
         model.addAttribute("nuevoServicio", new Servicio());
-        return "servicioForm";
+        return "serviciosForm";
     }
+
+    @PostMapping("/servicios/new/save")
+    public String serviciosSave(@ModelAttribute("nuevoServicio") Servicio nuevoServicio){
+        Servicio nuevo = new Servicio(nuevoServicio.getNombre(),nuevoServicio.getPrecio());
+        servicioRepository.save(nuevo);
+        return "redirect:/servicios";
+    }
+
+    @GetMapping("/servicios/{id}/edit")
+    public String serviciosEdit(Model model, @ModelAttribute("id") String id){
+        Servicio servicio =  servicioRepository.findById(id).get();
+        if(servicio != null){
+            model.addAttribute("servicios", servicio);
+            return "serviciosFormEditar";
+        }
+        return "redirect:/servicios";
+    }
+
+     @PostMapping("/servicios/{id}/edit/save")
+    public String serviciosEditSave(@ModelAttribute("servicio") Servicio nuevoServicio, @ModelAttribute("id") String id){
+        Servicio nuevo = new Servicio(nuevoServicio.getNombre(),nuevoServicio.getPrecio());
+        nuevo.setId(id);
+        servicioRepository.save(nuevo);
+        return "redirect:/servicios";
+    }
+
+    @GetMapping("/servicios/{id}/delete")
+    public String serviciosDelete(@ModelAttribute("id")String id) {
+        servicioRepository.deleteById(id);
+        return "redirect:/servicios";
+    }
+
 
 }
