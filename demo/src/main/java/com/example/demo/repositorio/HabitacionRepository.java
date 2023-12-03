29461328fe508e3cf3d6e9754166537f656ac371
s.umanap <s.umanap@uniandes.edu.co>
2023-12-03 15:48:03 -0500

    cambios html servicios

--- demo/src/main/java/com/example/demo/repositorio/HabitacionRepository.java
+++ demo/src/main/java/com/example/demo/repositorio/HabitacionRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.repositorio;
 
-import java.util.Date;
-import java.util.List;
+
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
--- demo/src/main/java/com/example/demo/repositorio/consumoRepository.java
+++ demo/src/main/java/com/example/demo/repositorio/consumoRepository.java
@@ -1,6 +1,5 @@
 package com.example.demo.repositorio;
 import java.util.List;
-import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.demo.modelo.consumo;
 
